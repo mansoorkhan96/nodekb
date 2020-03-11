352f70d5567106d54c2af594db6288a8f7f60ee5
Ms Global Developer <beingdeveloper96@gmail.com>
2020-03-11 18:51:23 +0500

    Lesson 4

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', function(error) {
+    console.log(error);
+});
+
+const app = express();
+
+let Article = require('./models/article');
+
+app.listen(3000, function() {
+    console.log('Server started at port 3000');
+});
