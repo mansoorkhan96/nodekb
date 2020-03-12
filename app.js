a76a8d449f897f6b83db0dee5281ec4f6d63d39b
Ms Global Developer <beingdeveloper96@gmail.com>
2020-03-12 12:17:31 +0500

    Part 7 Delete Remaining

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); 
 
 mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
@@ -15,7 +16,80 @@ db.on('error', function(error) {
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 let Article = require('./models/article');
+
+app.get('/', function(req, res) {
+    Article.find({}, function(error, articles) {
+        if(error) {
+            console.log(error);
+        } else {
+            res.json(articles);
+        }
+    });
+});
+
+app.get('/article/:id', function(req, res) {
+    Article.findById(req.params.id, function(error, article) {
+        res.json({status: true, data: article});
+    });
+});
+
+app.get('/article/edit/:id', function(req, res) {
+    Article.findById(req.params.id, function(error, article) {
+        res.json({status: true, data: article});
+    });
+});
+
+app.post('/articles/add', function(req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    
+    article.save(function(error) {
+        if(error) {
+            res.json({status: false, message: 'Could not add article'});
+        } else {
+            res.json({status: true, message: 'Article added successfully'});
+        }
+    });
+});
+
+// app.post('/article/update/:id', function(req, res) {
+//     let article = {};
+//     article.title = req.body.title;
+//     article.author = req.body.author;
+//     article.body = req.body.body;
+
+//     let query = {_id: req.params.id};
+
+//     Article.update(query, article, function(error) {
+//         if(error) {
+//             res.json({status: false, message: 'Could not update article'});
+//         } else {
+//             res.json({status: true, message: 'Article updated successfully'});
+//         }
+//     })
+// })
+
+app.post('/article/update/:id', function(req, res) {
+    Article.findByIdAndUpdate({_id: req.params.id}, {
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body
+    }, function(error, article) {
+        if(error) {
+            res.json({status: false, message: 'Could not update article'});
+        } else {
+            res.json({status: true, message: 'Article updated successfully'});
+        }
+    })
+})
 
 app.listen(3000, function() {
     console.log('Server started at port 3000');
