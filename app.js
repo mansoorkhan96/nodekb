88cb49e136150a8efe00ff68a03adb62f26255db
Ms Global Developer <beingdeveloper96@gmail.com>
2020-03-12 15:45:49 +0500

    Seprating routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); 
+const expressValidator = require('express-validator');
 
 mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
@@ -33,63 +34,11 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/article/:id', function(req, res) {
-    Article.findById(req.params.id, function(error, article) {
-        res.json({status: true, data: article});
-    });
-});
-
-app.get('/article/edit/:id', function(req, res) {
-    Article.findById(req.params.id, function(error, article) {
-        res.json({status: true, data: article});
-    });
-});
-
-app.post('/articles/add', function(req, res) {
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-    
-    article.save(function(error) {
-        if(error) {
-            res.json({status: false, message: 'Could not add article'});
-        } else {
-            res.json({status: true, message: 'Article added successfully'});
-        }
-    });
-});
-
-// app.post('/article/update/:id', function(req, res) {
-//     let article = {};
-//     article.title = req.body.title;
-//     article.author = req.body.author;
-//     article.body = req.body.body;
-
-//     let query = {_id: req.params.id};
-
-//     Article.update(query, article, function(error) {
-//         if(error) {
-//             res.json({status: false, message: 'Could not update article'});
-//         } else {
-//             res.json({status: true, message: 'Article updated successfully'});
-//         }
-//     })
-// })
-
-app.post('/article/update/:id', function(req, res) {
-    Article.findByIdAndUpdate({_id: req.params.id}, {
-        title: req.body.title,
-        author: req.body.author,
-        body: req.body.body
-    }, function(error, article) {
-        if(error) {
-            res.json({status: false, message: 'Could not update article'});
-        } else {
-            res.json({status: true, message: 'Article updated successfully'});
-        }
-    })
-})
+/**
+ * Routes
+ */
+let articles = './routes/article';
+app.use('/articles', articles);
 
 app.listen(3000, function() {
     console.log('Server started at port 3000');
---
+++ routes/article.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.router();
+
+let Article = require('../models/article');
+
+router.get('/:id', function(req, res) {
+    Article.findById(req.params.id, function(error, article) {
+        res.json({status: true, data: article});
+    });
+});
+
+router.get('/edit/:id', function(req, res) {
+    Article.findById(req.params.id, function(error, article) {
+        res.json({status: true, data: article});
+    });
+});
+
+router.post('/add', function(req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    
+    article.save(function(error) {
+        if(error) {
+            res.json({status: false, message: 'Could not add article'});
+        } else {
+            res.json({status: true, message: 'Article added successfully'});
+        }
+    });
+});
+
+// router.post('/update/:id', function(req, res) {
+//     let article = {};
+//     article.title = req.body.title;
+//     article.author = req.body.author;
+//     article.body = req.body.body;
+
+//     let query = {_id: req.params.id};
+
+//     Article.update(query, article, function(error) {
+//         if(error) {
+//             res.json({status: false, message: 'Could not update article'});
+//         } else {
+//             res.json({status: true, message: 'Article updated successfully'});
+//         }
+//     })
+// })
+
+router.post('/update/:id', function(req, res) {
+    Article.findByIdAndUpdate({_id: req.params.id}, {
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body
+    }, function(error, article) {
+        if(error) {
+            res.json({status: false, message: 'Could not update article'});
+        } else {
+            res.json({status: true, message: 'Article updated successfully'});
+        }
+    })
+});
+
+// router.delete('/:id', function(req, res) {
+//     let query = {_id: req.params.id};
+
+//     Article.remove(query, function(error) {
+//         if(error) {
+//             res.json({status: false, message: 'Could not delete article'});
+//         } else {
+//             res.json({status: true, message: 'Article deleted successfully'});
+//         }
+//     });
+// });
+
+router.delete('/:id', function(req, res) {
+    Article.findByIdAndDelete({_id: req.params.id}, function(error) {
+        if(error) {
+            res.json({status: false, message: 'Could not delete article'});
+        } else {
+            res.json({status: true, message: 'Article deleted successfully'});
+        }
+    });
+});
+
+module.exports = router;
